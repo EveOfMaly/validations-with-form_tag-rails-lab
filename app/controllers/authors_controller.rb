cd503671921a83e483f28b7daea6c44cc88d7fcd
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-03-01 11:24:36 -0500

    Completed assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,9 +8,33 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    #create a brand new author, not-yet-validated author object from the from 
+    @author = Author.new(author_params)
 
-    redirect_to author_path(@author)
+    #run the validations without attempting to hit the databases, returning true if valid and false others
+    if @author.valid? 
+       @author.save #save to database
+       #throw away @author and the show action read from the database
+       redirect_to author_path(@author)
+    else
+      #if invalid hold on to @author which has erros and renter it to the :new page which knows how to display the erros 
+      render :new
+    end
+
+  
+  end
+
+  def edit 
+    @author = Author.find(params[:id])
+  end
+
+
+  def update
+     if @author.update(author_params) 
+      redirect_to author_path(@author)
+     else 
+      render :edit
+     end
   end
 
   private
